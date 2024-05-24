1d8b077cadb603afb70a2734fbc7a190b9b6b273
apil <078bct017.apil@pcampus.edu.np>
2024-05-24 13:15:54 +0545

    fix base_link to cam transform by taking rf from config

--- transforms/base2cam_optical.py
+++ transforms/base2cam_optical.py
@@ -4,15 +4,19 @@ import rclpy
 from rclpy.node import Node
 
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
-
-T_BASE2CAM = [0.0, 0.0, 0.0]
-QUAT_BASE2CAM = [0.0, 0.0, 0.0, 1.0]
+import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 class StaticFramePublisher(Node):
   def __init__(self):
     super().__init__('static_base2cam_tf_publisher')
 
     self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+    self.declare_parameter("translation", [0.0, 0.0, 0.0])
+    self.declare_parameter("ypr", [0.0, 0.0, 0.0])
+
+    self.translation_ = self.get_parameter("translation").get_parameter_value().double_array_value
+    self.ypr_ = self.get_parameter("ypr").get_parameter_value().double_array_value
 
     # Publish static transforms once at startup
     self.make_transforms()
@@ -25,14 +29,16 @@ class StaticFramePublisher(Node):
     t.header.frame_id = 'base_link'
     t.child_frame_id = 'oakd_rgb_camera_optical_frame'
 
-    t.transform.translation.x = float(T_BASE2CAM[0])
-    t.transform.translation.y = float(T_BASE2CAM[1])
-    t.transform.translation.z = float(T_BASE2CAM[2])
+    t.transform.translation.x = float(self.translation_[0])
+    t.transform.translation.y = float(self.translation_[1])
+    t.transform.translation.z = float(self.translation_[2])
+
+    q_base2cam = R.from_euler("ZYX", self.ypr_, degrees=True).as_quat()
   
-    t.transform.rotation.x = QUAT_BASE2CAM[0]
-    t.transform.rotation.y = QUAT_BASE2CAM[1]
-    t.transform.rotation.z = QUAT_BASE2CAM[2]
-    t.transform.rotation.w = QUAT_BASE2CAM[3]
+    t.transform.rotation.w = q_base2cam[0]
+    t.transform.rotation.x = q_base2cam[1]
+    t.transform.rotation.y = q_base2cam[2]
+    t.transform.rotation.z = q_base2cam[3]
 
     self.tf_static_broadcaster.sendTransform(t)
 
