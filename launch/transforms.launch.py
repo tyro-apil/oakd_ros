dc6e9d8c0fb17ebecde16703df7fa874338a877b
apil <078bct017.apil@pcampus.edu.np>
2024-06-20 22:11:54 +0545

    fix: transforms -> tfs | change executable names

--- launch/transforms.launch.py
+++ launch/transforms.launch.py
@@ -19,25 +19,25 @@ def generate_launch_description():
     "namespace", default_value="", description="Name of the namespace"
   )
 
-  base2frontcam_optical_tf_node_cmd = Node(
+  base2cam_optical_tf_node_cmd = Node(
     package="oakd",
     namespace=namespace,
-    executable="base2frontcam_optical_tf",
-    name="base2frontcam_optical_tf",
+    executable="base2cam_optical_tf",
+    name="base2cam_optical_tf",
     parameters=[base2cam_config],
   )
 
-  frontcam_optical2frontcam_ros_tf_node_cmd = Node(
+  cam_optical2cam_ros_tf_node_cmd = Node(
     package="oakd",
     namespace=namespace,
-    executable="frontcam_optical2frontcam_ros_tf",
-    name="frontcam_optical2frontcam_ros_tf",
+    executable="cam_optical2cam_ros_tf",
+    name="cam_optical2cam_ros_tf",
   )
 
   ld = LaunchDescription()
 
   ld.add_action(namespace_cmd)
-  ld.add_action(base2frontcam_optical_tf_node_cmd)
-  ld.add_action(frontcam_optical2frontcam_ros_tf_node_cmd)
+  ld.add_action(base2cam_optical_tf_node_cmd)
+  ld.add_action(cam_optical2cam_ros_tf_node_cmd)
 
   return ld
